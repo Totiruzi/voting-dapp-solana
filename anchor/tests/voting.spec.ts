53ee0379afd2de599804aab80011119a57c6b724
totiruziAlphaO <totiruzi@yahoo.com>
2024-12-24 16:22:40 +0300

    Initialize Voting Account And Poll

---
+++ anchor/tests/voting.spec.ts
@@ -0,0 +1,13 @@
+import * as anchor from '@coral-xyz/anchor'
+import {Program} from '@coral-xyz/anchor'
+import {Keypair} from '@solana/web3.js'
+import {Voting} from '../target/types/voting'
+
+describe('voting', () => {
+  // Configure the client to use the local cluster.
+  it('Initialize Voting', async () => {
+    
+  })
+
+  
+})
